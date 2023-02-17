7ca349754696e6db3cf7b2a0c727b4ecccb603ea
CristRM27 <123188890+CristRM27@users.noreply.github.com>
2023-02-17 11:51:13 -0300

    Edit view for Writer

--- controllers/writerController.js
+++ controllers/writerController.js
@@ -34,24 +34,27 @@ async function createArticle(req, res) {
 
 async function editForm(req, res) {
   const article = await Article.findByPk(req.params.id);
-  res.render("edit", { article });
+  res.render("editWriter", { article });
 }
 
 async function editArticle(req, res) {
-  const newArticle = await Article.update(
-    {
-      title: `${req.body.title}`,
-      content: `${req.body.content}`,
-      date: {
-        type: Sequelize.DATE,
-        field: "updated_at",
+  const form = formidable({
+    multiples: true,
+    uploadDir: __dirname + "/../public/img",
+    keepExtensions: true,
+  });
+  form.parse(req, async (err, fields, files) => {
+    const newArticle = await Article.update(
+      {
+        title: fields.title,
+        content: fields.content,
+        image: files.image.newFilename,
+        userId: req.user.id,
       },
-      image: `${req.body.image}`,
-      author: `${req.body.author}`,
-    },
-    { where: { id: req.params.id } },
-  );
-  return res.redirect("/admin");
+      { where: { id: req.params.id } },
+    );
+    return res.redirect("/writer");
+  });
 }
 
 async function deleteArticle(req, res) {
